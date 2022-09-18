2c950429c7febf4d1857863214698febceaac486
DevNisah <issahnelson2@gmail.com>
2022-09-18 15:52:38 +0200

    my personal portfolio site

---
+++ main.js
@@ -0,0 +1,23 @@
+
+// Open and close menu bar for mobile and tablet view
+let open = document.querySelector(".fa-bars");
+      let close = document.querySelector(".fa-times");
+
+      let navbar = document.querySelector(".nav-links");
+
+      open.addEventListener("click", () => {
+        navbar.style.left = "50%";
+      });
+      close.addEventListener("click", () => {
+        navbar.style.left = "0%";
+      });
+
+// change nav links color
+let changeColor = document.querySelector(".change-color")
+      window.addEventListener("scroll", () => {
+        if(window.scrollY >= 3){
+            changeColor.classList.add(".changed")
+        } else{
+            changeColor.classList.remove(".changed")
+        }
+      })
